995165ce4349989f1c335ed57b1e11db331f5a43
charit <charit@gmail.com>
2021-12-18 21:13:52 -0500

    Infinite Scroll Using JavaScript

--- script.js
+++ script.js
@@ -1,19 +1,69 @@
-//unsplash API
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
+// Unsplash API
 const count = 10;
 const apiKey = '59dKnT1j0NDBVv3QEzvaUdanNbGN9_39SISasHajJDs';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
-//Get photos from Unsplash API
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes)
+{
+    for(const key in attributes)
+    {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+// Create Elements For Links & Photos, Add to DOM
+function displayPhotos()
+{
+    // Run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        
+        // Create <a> to link to unsplash
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+
+        //Create <img> for photo
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+        
+        // Put <img> inside <a> then put both inside imageContainer Element
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+
+// Get photos from Unsplash API
 async function getPhotos()
 {
     try{
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
+        photosArray = await response.json();
+        displayPhotos();
     }catch(error){
-        //Display error.
+        // Display error.
     }
 }
 
-//on Load
+//
+window.addEventListener('scroll', () =>{
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000)
+    {
+        getPhotos();
+        console.log('loaded');
+    }
+});
+
+// On Load
 getPhotos();
